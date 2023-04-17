29b0dc8469833b71d3df725e198ea6e33fce3022
Марат Джеманкулов <m.dzhemankulov@profsoft.pro>
2023-04-18 01:05:02 +0400

    Add gradle version catalog and detekt. Change gradle.properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,26 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.jvm) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.detekt)
+    alias(libs.plugins.ksp)
     base
-    id("com.android.application") version "8.1.0-alpha06" apply false
-    id("com.android.library") version "8.1.0-alpha06" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.20" apply false
+}
+
+val detektFormatting = libs.detekt.formatting
+val detektCompose = libs.detekt.rules.compose
+
+subprojects {
+    apply {
+        plugin("io.gitlab.arturbosch.detekt")
+    }
+    detekt {
+        config = rootProject.files("config/detekt/detekt.yml")
+        baseline = rootProject.file("config/detekt/baseline.xml")
+    }
+    dependencies {
+        detektPlugins(detektFormatting)
+        detektPlugins(detektCompose)
+    }
 }
